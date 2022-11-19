b978526bc4ad658b862992c95642fb4fd6abe77f
GitHub Action <action@github.com>
2022-11-19 13:35:47 +0000

    Template cleanup

--- src/main/kotlin/com/github/icankeep/intellijplugindemo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/icankeep/intellijplugindemo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.icankeep.intellijplugindemo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.icankeep.intellijplugindemo.MyBundle
 
 class MyApplicationService {
 
